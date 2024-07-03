821f979d26ca9348b20f21375a6099b4b64f3742
Subrota Debnath <subrotadebnath1807@gmail.com>
2024-07-03 14:15:54 +0600

    lsp config add for language server  with mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,29 +3,28 @@ return {
     "williamboman/mason.nvim",
     lazy = false,
     config = function()
-      require("mason").setup()sudo apt install gnome-shell-extension-manager
+      require("mason").setup()
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
+     ensure_installed = { "lua_ls", "rust_analyzer" },
     opts = {
       auto_install = true,
     },
   },
+   {'hrsh7th/nvim-cmp'},
+  {'hrsh7th/cmp-nvim-lsp'},
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
+
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
+ 
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
