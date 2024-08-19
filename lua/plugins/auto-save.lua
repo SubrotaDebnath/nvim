8aa80f85843bf882cbbbca2c84f0244d4c146f1b
Subrota Debnath <subrotadebnath1807@gmail.com>
2024-08-19 14:22:43 +0600

    git ignore file add

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,10 +1,10 @@
 return {
-	"okuuva/auto-save.nvim",
-	cmd = "ASToggle", -- optional for lazy loading on command
-	event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-	opts = {
-		-- your config goes here
-		-- or just leave it empty :)
-		-- testing auto save
-	},
+  "okuuva/auto-save.nvim",
+  cmd = "ASToggle",                        -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  opts = {
+    -- your config goes here
+    -- or just leave it empty :)
+    -- testing auto save
+  },
 }
