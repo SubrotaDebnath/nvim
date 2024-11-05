562f76081173adc12b9000a060a4754a4b01bfd3
Subrota Debnath <subrotadebnath1807@gmail.com>
2024-11-05 17:29:38 +0600

    trying to show neotree hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require('neo-tree').setup({
+      default_behavior = {
+        open_tree = {
+          action = 'expand',
+          expand_level = 1,
+        },
+        select_file = {
+          action = 'open',
+        },
+      },
+      filesystem = {
+        follow_current_file = true,
+        show_hidden = true, -- This line shows hidden files
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    vim.keymap.set("n", "<C-b>", "<CMD>Neotree toggle<CR>", {})
+  end,
 }
