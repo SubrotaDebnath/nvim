b200c3485b2e7f22072015409d5d699f40847266
Subrota Debnath <subrotadebnath1807@gmail.com>
2024-12-11 16:47:52 +0600

    telescope modified
    anywhare to neovim config navigation setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,11 +22,18 @@ return {
           },
         },
       })
+      -- goto neovim config
+      vim.keymap.set("n", "<leader>en",
+        function()
+          require('telescope.builtin').find_files {
+            cwd = vim.fn.stdpath("config"),
+          }
+        end)
+
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-
       require("telescope").load_extension("ui-select")
     end,
   },
